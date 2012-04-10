90debe277dda1ee562479e52a84d8fbc8a1d03ae
Tony Leung <heyytonytony@gmail.com>
2012-04-10 00:47:00 -0400

    improved pause, framework for unit management

--- shoppe/src/shoppe/android/ShoppeActivity.java
+++ shoppe/src/shoppe/android/ShoppeActivity.java
@@ -1,6 +1,10 @@
 package shoppe.android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,7 +20,7 @@ import android.widget.ViewFlipper;
 
 public class ShoppeActivity extends Activity implements OnTouchListener
 {
-	
+
 	private float oldTouchValue;
 	private ShoppeView shoppeView;
 	private ShoppeThread shoppeThread;
@@ -32,12 +36,12 @@ public class ShoppeActivity extends Activity implements OnTouchListener
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		Window window = getWindow();
 		window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-		
+
 		shoppeView = (ShoppeView)findViewById(R.id.shoppeView);
 		shoppeThread = shoppeView.getThread();
 		viewFlipper = (ViewFlipper)findViewById(R.id.viewFlipper);
-        findViewById(R.id.hide_inv).setOnTouchListener(this);
-        findViewById(R.id.show_inv).setOnTouchListener(this);
+		findViewById(R.id.hide_inv).setOnTouchListener(this);
+		findViewById(R.id.show_inv).setOnTouchListener(this);
 	}
 
 	public void pauseButton(View view)
@@ -48,62 +52,151 @@ public class ShoppeActivity extends Activity implements OnTouchListener
 		// toggle text displayed
 		if(button.getText().toString().equals(getResources().getString(R.string.pause)))
 		{
-			button.setText(getResources().getString(R.string.play));
+			showDialog(0);
 			shoppeThread.setRunning(false);
-		}
-		else
-		{
-			button.setText(getResources().getString(R.string.pause));
-			shoppeThread.setRunning(true);
 		}
 	}
 
 	public void artisanButton(View view)
 	{
 		// TODO:Implement artisanButton
+		Button button = (Button)view;
+		if(button.getId() == R.id.artisan0Button)
+		{
+			Log.d("Activity", "Artisan 1 pressed");
+			showDialog(1);
+			shoppeThread.setRunning(false);
+		}
+		if(button.getId() == R.id.artisan1Button)
+		{
+			Log.d("Activity", "Artisan 2 pressed");
+			showDialog(2);
+			shoppeThread.setRunning(false);
+		}
+		if(button.getId() == R.id.artisan2Button)
+		{
+			Log.d("Activity", "Artisan 3 pressed");
+			showDialog(3);
+			shoppeThread.setRunning(false);
+		}
+		if(button.getId() == R.id.artisan3Button)
+		{
+			Log.d("Activity", "Artisan 4 pressed");
+			showDialog(4);
+			shoppeThread.setRunning(false);
+		}
 	}
 
 	private boolean onButtonTouchEvent(MotionEvent mEvent)
-    {
-        if (inv == null)
-            return false;
-        switch (mEvent.getAction())
-        {
-            case MotionEvent.ACTION_DOWN:
-            {
-                oldTouchValue = mEvent.getY();
-                break;
-            }
-            case MotionEvent.ACTION_UP:
-            {
-                float currentY = mEvent.getY();
-                final float diff = oldTouchValue - currentY;
-                if ((diff < -100) && (inv.getId() == R.id.hide_inv))
-                {
-                    //Up --> Bottom
-                    viewFlipper.setInAnimation(AccordionAnimation.inFromTopAnimation());
-                    viewFlipper.setOutAnimation(AccordionAnimation.outToBottomAnimation());
-                    viewFlipper.showNext();
-                }
-                else if ((diff > 100) && (inv.getId() == R.id.show_inv))
-                {
-                    //Bottom --> Up
-                    viewFlipper.setInAnimation(AccordionAnimation.inFromBottomAnimation());
-                    viewFlipper.setOutAnimation(AccordionAnimation.outToTopAnimation());
-                    viewFlipper.showPrevious();
-                }
-                break;
-            }
-        }
-        inv = null;
-        return true;
-    }
-	
+	{
+		if(inv == null)
+			return false;
+		switch(mEvent.getAction())
+		{
+			case MotionEvent.ACTION_DOWN:
+			{
+				oldTouchValue = mEvent.getY();
+				break;
+			}
+			case MotionEvent.ACTION_UP:
+			{
+				float currentY = mEvent.getY();
+				final float diff = oldTouchValue - currentY;
+				if((diff < -100) && (inv.getId() == R.id.hide_inv))
+				{
+					// Up --> Bottom
+					viewFlipper.setInAnimation(AccordionAnimation.inFromTopAnimation());
+					viewFlipper.setOutAnimation(AccordionAnimation.outToBottomAnimation());
+					viewFlipper.showNext();
+				}
+				else if((diff > 100) && (inv.getId() == R.id.show_inv))
+				{
+					// Bottom --> Up
+					viewFlipper.setInAnimation(AccordionAnimation.inFromBottomAnimation());
+					viewFlipper.setOutAnimation(AccordionAnimation.outToTopAnimation());
+					viewFlipper.showPrevious();
+				}
+				break;
+			}
+		}
+		inv = null;
+		return true;
+	}
+
 	@Override
 	public boolean onTouch(View view, MotionEvent mEvent)
 	{
 		inv = (ImageView)view;
-        final boolean result = this.onButtonTouchEvent(mEvent);
-        return result;
+		final boolean result = this.onButtonTouchEvent(mEvent);
+		return result;
+	}
+
+	protected Dialog onCreateDialog(int id)
+	{
+		Context mContext = shoppeView.getContext();
+		Dialog dia = null;
+		switch(id)
+		{
+			case 0:
+				// game paused
+				AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.setMessage("Game paused!").setCancelable(false).setNeutralButton("Unpause!", new DialogInterface.OnClickListener()
+				{
+					public void onClick(DialogInterface dialog, int id)
+					{
+						shoppeThread.setRunning(true);
+						dialog.dismiss();
+					}
+				});
+				AlertDialog alert = builder.create();
+				dia = alert;
+				break;
+
+			case 1:
+				// artisan 1
+				dia = new Dialog(mContext);
+				
+				dia.setContentView(R.layout.artisan_dialog);
+				dia.setTitle("Artisan 1 Management");
+
+				TextView text = (TextView) dia.findViewById(R.id.text);
+				text.setText("Hello, this is a custom dialog!");
+
+
+				break;
+				
+			case 2:
+				// artisan 2
+				dia = new Dialog(mContext);
+				
+				dia.setContentView(R.layout.artisan_dialog);
+				dia.setTitle("Artisan 2 Management");
+				TextView text1 = (TextView) dia.findViewById(R.id.text);
+				text1.setText("Hello, this is a custom dialog!");
+				break;
+				
+			case 3:
+				// artisan 3
+				dia = new Dialog(mContext);
+				
+				dia.setContentView(R.layout.artisan_dialog);
+				dia.setTitle("Artisan 3 Management");
+				TextView text2 = (TextView) dia.findViewById(R.id.text);
+				text2.setText("Hello, this is a custom dialog!");
+				break;
+				
+			case 4:
+				// artisan 4
+				dia = new Dialog(mContext);
+				
+				dia.setContentView(R.layout.artisan_dialog);
+				dia.setTitle("Artisan 4 Management");
+				TextView text3 = (TextView) dia.findViewById(R.id.text);
+				text3.setText("Hello, this is a custom dialog!");
+				break;
+		}
+
+		return dia;
+
 	}
 }
--- shoppe/src/shoppe/android/ShoppeThread.java
+++ shoppe/src/shoppe/android/ShoppeThread.java
@@ -20,9 +20,9 @@ import android.graphics.drawable.Drawable;
 
 public class ShoppeThread extends Thread
 {
-	 /** Indicate whether the surface has been created and is ready to draw */
-    private boolean mRun = false;
-	
+	/** Indicate whether the surface has been created and is ready to draw */
+	private boolean mRun = false;
+
 	/** Width of the grid of gridElements **/
 	private static int gridWidth = 8;
 	/** Height of the grid of gridElements **/
@@ -86,42 +86,43 @@ public class ShoppeThread extends Thread
 
 	/** The list of artisans working in the shop **/
 	private LinkedList<Artisan> artisanList = new LinkedList<Artisan>();
-	
+
 	/** The list of items owned by the player **/
 	private LinkedList<Item> inventoryList = new LinkedList<Item>();
 
 	private SurfaceHolder surfaceHolder = null;
-	
+
 	/** Used to keep track of update cycles **/
 	private long beginTime;
-	
+
 	private boolean[][] tileOccupied = new boolean[gridHeight][gridWidth];
-	
+
 	/** The amount of time in seconds that patron positions are updated **/
 	private static final int patronUpdateInterval = 2;
-	
+
 	/** Global list of possible items **/
 	private LinkedList<Item> itemList = new LinkedList<Item>();
-	
+
 	/** Defines how input is handled based on integer states **/
 	private int inputMode;
 	/** inputMode state where the user selects patrons **/
 	private static final int DEFAULT_INPUT = 0;
 	/** inputMode state where the user is placing an item on the shop floor **/
 	private static final int PLACEITEM_INPUT = 1;
-	
+
 	/** Paint to draw text **/
 	private Paint textPaint = new Paint();
-	
+
 	/** Paint to draw text background box **/
 	private Paint boxPaint = new Paint();
-	
+
 	/** Paint to draw border highlight **/
 	private Paint borderPaint = new Paint();
-	
+
 	/** Selected tile from user input to be highlighted **/
-	private int selectedX, selectedY;
-	
+	private int selectedX,
+			selectedY;
+
 	/** The patron currently engaged by the user **/
 	private Patron interactingPatron;
 
@@ -134,61 +135,63 @@ public class ShoppeThread extends Thread
 		plainTile = context.getResources().getDrawable(R.drawable.tile);
 		counterTile = context.getResources().getDrawable(R.drawable.countertile);
 		exclamationBubble = context.getResources().getDrawable(R.drawable.exclamation);
-		selectedX=selectedY=-1;
+		selectedX = selectedY = -1;
 		init();
 	}
 
 	public void init()
 	{
 		beginTime = System.currentTimeMillis();
-		//initialize occupied tiles on grid
-		for (int i = 0; i < gridHeight; i++) {
-			for (int j = 0; j < gridWidth; j++) {
+		// initialize occupied tiles on grid
+		for(int i = 0; i < gridHeight; i++)
+		{
+			for(int j = 0; j < gridWidth; j++)
+			{
 				tileOccupied[i][j] = false;
 			}
 		}
-		//initialize textPaint
+		// initialize textPaint
 		textPaint.setStyle(Paint.Style.FILL);
 		textPaint.setAntiAlias(true);
-		textPaint.setTextSize(tileHeight/4);
-		
-		//initialize boxPaint
+		textPaint.setTextSize(tileHeight / 4);
+
+		// initialize boxPaint
 		boxPaint.setARGB(255, 220, 220, 120);
-		
 
 		borderPaint.setStyle(Paint.Style.STROKE);
 		borderPaint.setStrokeWidth(2);
 		borderPaint.setColor(Color.BLACK);
-		
+
 		// testing draw method
-		//set counter tiles
+		// set counter tiles
 		tiles[1][1] = 1;
 		tiles[1][2] = 1;
 		tiles[4][4] = 1;
-		
-		//add patrons
+
+		// add patrons
 		addPatron(new Patron(3, 4, ShoppeConstants.potion, 100));
 		addPatron(new Patron(2, 3, ShoppeConstants.armor, 100));
-		
-		//test
+
+		// test
 		patronList.getLast().exclamation = true;
-		
-		//let's make some items
-		itemList.add(new Item(-1,-1,ShoppeConstants.weapon, ShoppeConstants.flail, 20, 4, 3, "Flail of Truthiness"));
+
+		// let's make some items
+		itemList.add(new Item(-1, -1, ShoppeConstants.weapon, ShoppeConstants.flail, 20, 4, 3, "Flail of Truthiness"));
 		itemList.add(new Item(-1, -1, ShoppeConstants.armor, ShoppeConstants.kite, 55, 5, 2, "Mangled Kite Armor"));
 		itemList.add(new Item(-1, -1, ShoppeConstants.potion, ShoppeConstants.poison, 100, 6, 5, "PBDE"));
 		itemList.add(new Item(-1, -1, ShoppeConstants.armor, ShoppeConstants.blah, 102, 4, 5, "Chipped Blah"));
-		
+
 	}
-	
-	public boolean addPatron(Patron patron) {
-		if (patron.xpos >= 0 && patron.xpos < gridWidth && patron.ypos >= 0 && patron.ypos < gridHeight
-				&& !tileOccupied[patron.ypos][patron.xpos]) {
+
+	public boolean addPatron(Patron patron)
+	{
+		if(patron.xpos >= 0 && patron.xpos < gridWidth && patron.ypos >= 0 && patron.ypos < gridHeight && !tileOccupied[patron.ypos][patron.xpos])
+		{
 			patronList.add(patron);
 			tileOccupied[patron.ypos][patron.xpos] = true;
 			return true;
 		}
-		//else
+		// else
 		return false;
 	}
 
@@ -210,69 +213,83 @@ public class ShoppeThread extends Thread
 			offsetX = (screenWidth - tileWidth * gridWidth) / 2;
 			offsetY = (screenHeight - tileHeight * gridHeight) / 2;
 
-			textPaint.setTextSize(tileHeight/4);
+			textPaint.setTextSize(tileHeight / 4);
 		}
 	}
-	
-	private void patronUpdate() {
+
+	private void patronUpdate()
+	{
 		Iterator<Patron> iterator = patronList.iterator();
 		Patron patron;
 		boolean[] availableDirections = new boolean[4];
 		int xpos, ypos;
-		if (System.currentTimeMillis() > beginTime + patronUpdateInterval*1000) {
-			while (iterator.hasNext()) {
-				for (int i = 0; i < 4; i++) {
+		if(System.currentTimeMillis() > beginTime + patronUpdateInterval * 1000)
+		{
+			while(iterator.hasNext())
+			{
+				for(int i = 0; i < 4; i++)
+				{
 					availableDirections[i] = false;
 				}
 				patron = iterator.next();
-				//only potentially move patrons that are not interacting with the user
-				if (patron.interacting == false) {
+				// only potentially move patrons that are not interacting with
+				// the user
+				if(patron.interacting == false)
+				{
 					xpos = patron.xpos;
 					ypos = patron.ypos;
-					if (ypos-1 >= 0) {
-						availableDirections[ShoppeConstants.up] = !tileOccupied[ypos-1][xpos];
+					if(ypos - 1 >= 0)
+					{
+						availableDirections[ShoppeConstants.up] = !tileOccupied[ypos - 1][xpos];
 					}
-					if (ypos+1 < gridHeight) {
-						availableDirections[ShoppeConstants.down] = !tileOccupied[ypos+1][xpos];
+					if(ypos + 1 < gridHeight)
+					{
+						availableDirections[ShoppeConstants.down] = !tileOccupied[ypos + 1][xpos];
 					}
-					if (xpos-1 >= 0) {
-						availableDirections[ShoppeConstants.left] = !tileOccupied[ypos][xpos-1]; 
+					if(xpos - 1 >= 0)
+					{
+						availableDirections[ShoppeConstants.left] = !tileOccupied[ypos][xpos - 1];
 					}
-					if (xpos+1 < gridWidth) {
-						availableDirections[ShoppeConstants.right] = !tileOccupied[ypos][xpos+1]; 
+					if(xpos + 1 < gridWidth)
+					{
+						availableDirections[ShoppeConstants.right] = !tileOccupied[ypos][xpos + 1];
 					}
 					int moveDirection = patron.move(availableDirections);
-					if (moveDirection > -1) {	//update tileOccupied
+					if(moveDirection > -1)
+					{ // update tileOccupied
 						tileOccupied[ypos][xpos] = false;
-						switch (moveDirection) {
-						case ShoppeConstants.up: 
-							tileOccupied[ypos-1][xpos] = true;
-							break;
-						case ShoppeConstants.down:
-							tileOccupied[ypos+1][xpos] = true;
-							break;
-						case ShoppeConstants.left:
-							tileOccupied[ypos][xpos-1] = true;
-							break;
-						case ShoppeConstants.right:
-							tileOccupied[ypos][xpos+1] = true;
-							break;
+						switch(moveDirection)
+						{
+							case ShoppeConstants.up:
+								tileOccupied[ypos - 1][xpos] = true;
+								break;
+							case ShoppeConstants.down:
+								tileOccupied[ypos + 1][xpos] = true;
+								break;
+							case ShoppeConstants.left:
+								tileOccupied[ypos][xpos - 1] = true;
+								break;
+							case ShoppeConstants.right:
+								tileOccupied[ypos][xpos + 1] = true;
+								break;
 						}
 					}
 				}
-				//else do nothing
+				// else do nothing
 			}
 			beginTime = System.currentTimeMillis();
 		}
 	}
-	
-	public boolean buyItem(Item item) {
-		if (funds-item.value >= 0) {
+
+	public boolean buyItem(Item item)
+	{
+		if(funds - item.value >= 0)
+		{
 			funds -= item.value;
 			inventoryList.add(item);
 			return true;
 		}
-		//else
+		// else
 		return false;
 	}
 
@@ -281,7 +298,8 @@ public class ShoppeThread extends Thread
 	{
 		while(true)
 		{
-			if (mRun) {
+			if(mRun)
+			{
 				patronUpdate();
 				Canvas canvas = null;
 				try
@@ -302,7 +320,7 @@ public class ShoppeThread extends Thread
 			}
 		}
 	}
-	
+
 	public void setRunning(boolean b)
 	{
 		mRun = b;
@@ -334,12 +352,13 @@ public class ShoppeThread extends Thread
 				}
 			}
 		}
-		//draw selected tile highlight
-		if (selectedX > -1 && selectedY > -1) {
-			tileX = selectedX*tileWidth + offsetX;
-			tileY = selectedY*tileHeight + offsetY;
+		// draw selected tile highlight
+		if(selectedX > -1 && selectedY > -1)
+		{
+			tileX = selectedX * tileWidth + offsetX;
+			tileY = selectedY * tileHeight + offsetY;
 			borderPaint.setColor(Color.YELLOW);
-			canvas.drawRect(tileX, tileY, tileX+tileWidth, tileY+tileHeight, borderPaint);
+			canvas.drawRect(tileX, tileY, tileX + tileWidth, tileY + tileHeight, borderPaint);
 		}
 	}
 
@@ -378,68 +397,80 @@ public class ShoppeThread extends Thread
 
 		drawGrid(canvas);
 		drawPatrons(canvas);
-		//border
+		// border
 		borderPaint.setColor(Color.BLACK);
-		canvas.drawRect(8, 8, tileWidth*2+2, 20+tileHeight/2+2, borderPaint);
-		//background
-		canvas.drawRect(10, 10, tileWidth*2, 20+tileHeight/2, boxPaint);
-		//text
-		canvas.drawText("Funds: " + funds, 20, tileHeight/2, textPaint);
+		canvas.drawRect(8, 8, tileWidth * 2 + 2, 20 + tileHeight / 2 + 2, borderPaint);
+		// background
+		canvas.drawRect(10, 10, tileWidth * 2, 20 + tileHeight / 2, boxPaint);
+		// text
+		canvas.drawText("Funds: " + funds, 20, tileHeight / 2, textPaint);
 	}
 
-	public boolean onTouch(MotionEvent event) {
+	public boolean onTouch(MotionEvent event)
+	{
 		float inputX, inputY;
 		int tileX, tileY;
 		Iterator<Patron> iterator;
 		Patron patron;
-		//Log.v("onTouch","Input event " + event.getAction());
-		if (event.getAction() == MotionEvent.ACTION_DOWN){	//pressing input is received
+		// Log.v("onTouch","Input event " + event.getAction());
+		if(event.getAction() == MotionEvent.ACTION_DOWN)
+		{ // pressing input is received
 			inputX = event.getX();
 			inputY = event.getY();
-			//Log.v("onTouch","getX: " + inputX + " getY: " + inputY);
-			//determine if input event is within the bounds of the grid
-			if (interactingPatron != null) {
+			// Log.v("onTouch","getX: " + inputX + " getY: " + inputY);
+			// determine if input event is within the bounds of the grid
+			if(interactingPatron != null)
+			{
 				interactingPatron.endInteraction();
 				interactingPatron = null;
-				//TODO: call interactingPatron.endInteraction() in the appropriate place
+				// TODO: call interactingPatron.endInteraction() in the
+				// appropriate place
 				selectedX = -1;
 				selectedY = -1;
 			}
-			if (inputX >= offsetX && inputY > offsetY && inputX < offsetX+gridWidth*tileWidth && inputY < offsetY+gridHeight*tileHeight) {
-				//find the corresponding tile location
-				tileX = (int) ((1.0*inputX-offsetX)/tileWidth);
-				tileY = (int) ((1.0*inputY-offsetY)/tileHeight);
-				//Log.v("onTouch","x: " + tileX + " y: " + tileY);
-				switch (inputMode) {
-				case DEFAULT_INPUT:
-					//determine if there might exist a patron at the given tile
-					if (tileOccupied[tileY][tileX]) {
-						//search through patron list for potential patron at this location
-						iterator = patronList.iterator();
-						while (iterator.hasNext()) {
-							patron = iterator.next();
-							if (patron.xpos == tileX && patron.ypos == tileY) {
-								//highlight the tile
-								selectedX=tileX;
-								selectedY=tileY;
-								//start interaction
-								interactingPatron = patron;
-								if (patron.startInteraction()) {
+			if(inputX >= offsetX && inputY > offsetY && inputX < offsetX + gridWidth * tileWidth && inputY < offsetY + gridHeight * tileHeight)
+			{
+				// find the corresponding tile location
+				tileX = (int)((1.0 * inputX - offsetX) / tileWidth);
+				tileY = (int)((1.0 * inputY - offsetY) / tileHeight);
+				// Log.v("onTouch","x: " + tileX + " y: " + tileY);
+				switch(inputMode)
+				{
+					case DEFAULT_INPUT:
+						// determine if there might exist a patron at the given
+						// tile
+						if(tileOccupied[tileY][tileX])
+						{
+							// search through patron list for potential patron
+							// at this location
+							iterator = patronList.iterator();
+							while(iterator.hasNext())
+							{
+								patron = iterator.next();
+								if(patron.xpos == tileX && patron.ypos == tileY)
+								{
+									// highlight the tile
+									selectedX = tileX;
+									selectedY = tileY;
+									// start interaction
 									interactingPatron = patron;
+									if(patron.startInteraction())
+									{
+										interactingPatron = patron;
+									}
+									break;
 								}
-								break;
 							}
 						}
-					}
-					break;
-				case PLACEITEM_INPUT:
-					//TODO: implement item placement on shop floor
-					break;
+						break;
+					case PLACEITEM_INPUT:
+						// TODO: implement item placement on shop floor
+						break;
 				}
 			}
 			return true;
 		}
-		//else
+		// else
 		return false;
 	}
 }
